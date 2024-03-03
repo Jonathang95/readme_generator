ac221869e8e9b05dde5bc227e42e76e15d4cee86
unknown <jongarcia.realty@gmail.com>
2024-03-03 10:18:37 -0700

    added all code needed to index.js and generateMarkdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of the purpose and functionality of your project.'
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: '"Please provide the relative path to the image you want to use as the screenshot.'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'please select a license',
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'Please list any installation requirements for your project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What steps do users need to follow to utilize your project.'
+    },
+    {
+        type: 'input',
+        name: 'creator',
+        message: 'Write your Github username'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your email'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Please list any contributors (Github username).',
+        default: "",
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide guidelines for testing your project.'
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(filename, data) {
+    return fs.writeFileSync(path.join(process.cwd(), filename), data);
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {  
+    inquirer.prompt(questions).then((data) => {
+        writeToFile('README.md', generateMarkdown(data));
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,57 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+  function renderLicenseBadge(license) {
+    if (license !== "none") {
+      return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    }
+    return "";
+  }
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseURLs = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'APACHE2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'Boost1.0': 'https://opensource.org/licenses/BSL-1.0',
+    'MPL2.0': 'https://opensource.org/licenses/MPL-2.0',
+    'BSD2': 'https://opensource.org/licenses/BSD-2-Clause',
+    'BSD3': 'https://opensource.org/licenses/BSD-3-Clause',
+  }; if (license!== "none") {
+  return `![Github license](${licenseURLs[license]})`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { 
+  if (license!== "none") {
+    return `## License 
+    This application is covered under the ${license} license.`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Deployed Application URL
+${data.link}
+## Screenshot
+![alt-text](${data.screenshot})
+## Installation
+${data.installation}
+## How to Use This Application:
+${data.usage}
+## Contributors
+${data.contributors}
+## Testing
+${data.tests}
+## Questions
+Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
 `;
 }
-
-module.exports = generateMarkdown;
+module.exports = generateMarkdown
